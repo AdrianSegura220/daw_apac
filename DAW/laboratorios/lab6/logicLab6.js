78764b9b15f348ec0aef103b5a82576518c071b5
Adrian segura <adrianseglor@gmail.com>
2019-02-10 00:04:09 -0600

    casi finalizar, resta drag & drop lab 6

--- DAW/laboratorios/lab6/logicLab6.js
+++ DAW/laboratorios/lab6/logicLab6.js
@@ -30,14 +30,50 @@ function masInfo(){
     
 }
 
-function init(){
-    
+function init(event){
+    console.log("x: "+ event.pageX);
+    console.log("y: "+ event.pageY);
 }
+function cambioLetra(){
+    cont4++;
+    if(cont3 % 2 == 0){
+        document.getElementById("hide").style.visibility = "visible";
+    }else{
+        document.getElementById("hide").style.visibility = "hidden";        
+    }
+}
+let cont4 = 1;
 let cont3 = 1;
 let cont2 = 1;
 let cont = 1;
+let awayFK = false;
+function afk(){
+    
+    awayFK = confirm("¿Sigues ahí?");
+    setTimeout(confirmar(),10000);
+}
+function confirmar(){
+    if(!awayFK){
+        alert("Al parecer no sigues ahí..."+'<br>'+"La utilidad de esto sería por ejemplo para una aplicación de streaming de video. De esta manera no se utilizarían recursos en vano. Por ejemplo Netflix después de detectar algún patrón fuera de lo habitual en una sesión. Por ejemplo deteniendo el stream y poniendo una pantalla negra o algo por el estilo.");
+    }
+}
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
 document.getElementById("esconder").onclick = hideElement;
 document.getElementById("tutorial1").onmouseover = changeLetter;
 document.getElementById("tutorial1").onmouseout = changeLetter;
 document.getElementById("masInfo").onmouseover = masInfo;
 document.getElementById("masInfo").onmouseout = masInfo;
+document.getElementById("letterChange").onmouseover = cambioLetra;
+document.getElementById("letterChange").onmouseout = cambioLetra;
+setInterval(afk,5000);
